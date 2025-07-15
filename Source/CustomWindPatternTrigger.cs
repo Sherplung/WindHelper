67c0171500561693de7e8afc494d473835041651
Sherplung <alex.kalinin08s@gmail.com>
2025-07-15 13:36:22 -0400

    Added custom wind pattern trigger

---
+++ Source/CustomWindPatternTrigger.cs
@@ -0,0 +1,52 @@
+﻿using Celeste;
+using Celeste.Editor;
+using Celeste.Mod.Entities;
+using Celeste.Mod.WindHelper.Entities;
+using IL.Celeste.Mod.Registry.DecalRegistryHandlers;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.WindHelper;
+
+[CustomEntity("WindHelper/CustomWindPatternTrigger")]
+
+internal class CustomWindPatternTrigger : Trigger
+{
+    public WindController.Patterns Pattern;
+
+    private string instructions;
+
+    private bool onlyOnce;
+
+    private bool used;
+
+    public CustomWindPatternTrigger(EntityData data, Vector2 offset)
+        : base(data, offset)
+    {
+        instructions = data.Attr("instructions", "0.0,0.0,0.0");
+        onlyOnce = data.Bool("onlyOnce", true);
+        used = false;
+    }
+
+    public override void OnEnter(Player player)
+    {
+        if (!used)
+        {
+            base.OnEnter(player);
+            ExtendedWindController windController = base.Scene.Entities.FindFirst<ExtendedWindController>();
+            if (windController == null)
+            {
+                windController = new ExtendedWindController(Pattern);
+                base.Scene.Add(windController);
+            }
+            windController.AddCustomWindPattern(instructions);
+            if (onlyOnce) { used = true; }
+        }
+    }
+}
